41ad7aa6ccaba1601eb81a2977d17b4c8a0b6d0f
Angela Cai <angcai@princeton.edu>
2024-07-04 17:13:26 +0800

    transformed neighborhood matrix. pasted r implementation of inla scale model

--- information_matrix.py
+++ information_matrix.py
@@ -5,3 +5,98 @@ Make information matrix of NYC zones based on COVID covariates and cases.
 Distance matrix is made of only neighboring zones, including zones connected by
 bridge or tunnel.
 '''
+
+import pandas as pd
+import numpy as np
+from scipy.sparse import csr_matrix, diags
+from scipy.sparse.linalg import inv as sparse_inv
+from scipy.sparse.csgraph import connected_components
+
+def inla_scale_model(Q, constr=None, eps=np.sqrt(np.finfo(float).eps)):
+    '''Copy of the inla.scale.model function in R. Found implementation online
+    at https://rdrr.io/github/inbo/INLA/src/R/scale.model.R. Comments are 
+    original R code.
+    
+    Returns scaled matrix Q given contraints and target equal value'''
+
+    ## return also the scaled marginal variances
+    # marg.var = rep(0, nrow(Q))
+    marg_var = np.zeros(Q.shape[0])
+
+    # Q = inla.as.sparse(Q)
+    Q = csr_matrix(Q)
+    
+    # inla.read.graph(Q))
+    n_components, labels = connected_components(csgraph=Q, directed=False, 
+                                                return_labels=True)
+    
+    # for(k in seq_len(g$cc$n)) {
+    for k in range(n_components):
+        # i = g$cc$nodes[[k]]
+        i = np.where(labels == k)[0]
+
+        # n = length(i)
+        n = len(i)
+
+        # QQ = Q[i, i, drop=FALSE]
+        QQ = Q[i, :][:, i].toarray()
+        
+        # if (n == 1) {
+        if n == 1:
+            # QQ[1, 1] = 1
+            QQ[0, 0] = 1
+
+            # marg.var[i] = 1
+            marg_var[i] = 1
+
+        # } else {
+        else:
+            # cconstr = constr
+            cconstr = constr
+
+            # if (!is.null(constr)) {
+            if constr is not None:
+                ## the GMRFLib will automatically drop duplicated constraints; how convenient...
+                # cconstr$A = constr$A[, i, drop = FALSE]
+                cconstr['A'] = constr['A'][:, i]
+
+                # eeps = eps
+                eeps = eps
+
+            # } else {
+            else:
+                # eeps = 0
+                eeps = 0
+            
+            # res = inla.qinv(QQ + Diagonal(n) * max(diag(QQ)) * eeps, constr = cconstr)
+            QQ = QQ + diags([np.max(np.diag(QQ)) * eeps], [0], shape=(n, n)).toarray()
+            res = sparse_inv(csr_matrix(QQ)).toarray()
+
+            # fac = exp(mean(log(diag(res))))
+            fac = np.exp(np.mean(np.log(np.diag(res))))
+
+            # QQ = fac * QQ
+            QQ = fac * QQ
+
+            # marg.var[i] = diag(res)/fac
+            marg_var[i] = np.diag(res) / fac
+        
+        # Q[i, i] = QQ
+        Q[i, :][:, i] = csr_matrix(QQ)
+    
+    # return (list(Q=Q, var = marg.var))
+    return {'Q': Q, 'var': marg_var}
+
+if __name__ == "__main__":
+    neighbor = pd.read_csv("bordering_neighbor.csv")
+    neighbor = neighbor.set_index("zcta")
+    aproximate_precision = neighbor.to_numpy()
+    diagonal = np.diag(np.sum(aproximate_precision, axis=0))
+    aproximate_precision = np.add(diagonal, aproximate_precision)
+
+    A = np.ones((1, aproximate_precision.shape[0])) 
+
+    constr = {'A': A}
+
+    scaled_result = inla_scale_model(aproximate_precision, constr)
+    print("Scaled Precision Matrix (Q):\n", scaled_result['Q'].toarray())
--- zcat.py
+++ zcat.py
@@ -1,9 +1,9 @@
 ''' 
 Create zcat matrix
 
-Return n by n location zcat neighbor matrix where TRUE means that n location
+Return n by n location zcat neighbor matrix where 1 means that n location
 borders or is connected by bridge/tunnel to the location at that index, and 
-FALSE means otherwise.
+0 means otherwise.
 '''
 import geopandas as gpd
 import pandas as pd
@@ -20,11 +20,24 @@ if __name__ == "__main__":
     zcat_df = pd.DataFrame.from_dict(zcat_dict)
     zcat_df = zcat_df.set_index(nyc["zcta"])
 
+    # Test print
+    print(zcat_df.at["10305", "11209"])
+
     # Change values to true if there's a bridge or tunnel
-    # connections = {}
-    # for area in connections:
-    #     zcat_df.loc[area, connections[area]] = 1
-    #     zcat_df.loc[connections[area], area] = 1
+    connections = {
+        "10305": "11209", "10004": "11231", "10038": "11201", "11201": "10002", 
+        "10002": "11211", "10017": "11101", "11101": "10044", "10044": "10021", 
+        "11102": "10035", "11368": "11354", "11357": "10465", "10465": "11360", 
+        "10475": "10464", "10037": "10451", "10454": "10035", "10451": "10039", 
+        "10452": "10039", "10039": "10453", "10453": "10033", "10034": "10468",
+        "10463": "10034", "11697": "11234"
+    }
+    for area in connections:
+        zcat_df.loc[area, connections[area]] = 1
+        zcat_df.loc[connections[area], area] = 1
+        
+    # Test print
+    print(zcat_df.loc["10305", "11209"])
 
     # Save to cvs
-    zcat_df.to_csv("bordering_zcat.csv")
+    zcat_df.to_csv("connections_zcat.csv")
